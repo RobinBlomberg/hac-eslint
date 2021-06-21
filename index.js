a9c2cf0c3c2598d38508cc7032a033f95ec392e0
Robin Blomberg <robin.blomberg@hiq.se>
2021-06-21 14:06:54 +0200

    Enable "no-unused-expressions" in TS files

--- index.js
+++ index.js
@@ -87,7 +87,6 @@ module.exports = {
          * @see https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
          */
         'no-undef': off,
-        'no-unused-expressions': off,
         'no-unused-vars': off,
         'no-use-before-define': off,
         radix: off,
